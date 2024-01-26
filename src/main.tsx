8ab4e086d706eb900aee5bc966f5162e85895277
crow <erickaricari@gmail.com>
2024-01-26 18:53:42 -0400

    five commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import { Toaster } from "@/components/ui/toaster"
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
@@ -6,7 +5,6 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Toaster/>
     <App />
   </React.StrictMode>,
 )
